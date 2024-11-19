79dbd39245e9d896fd02767e76b6506d853dcd5c
Luka <rogluka@gmail.com>
2024-11-19 00:41:34 -0800

    add input field and button and hint display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import CanadaMap from "./assets/CanadaMap.jpg";
+import React, { useState } from 'react';
 
 function App() {
+  const [hints, setHints] = useState(3);
+
   return (
     <div className="App">
       <div className="Title">
-        <h1 classNam="Title">Culturedle</h1>
+        <h1 className="Title">Culturedle</h1>
       </div>
       <div className="Hints">
-        <img className="img" src={CanadaMap} alt="CanadaMap"/>
+        <img className="img" src={CanadaMap} alt="CanadaMap" />
+        <h1> Hints remaining: {hints} </h1>
       </div>
-      <div className="Textbox"></div>
+      <span>
+        <input type="text" className="input-field"/> <button className="btn" onClick={() => setHints(Math.max(hints-1, 0))}> Test </button>
+      </span>
     </div>
   );
 }
