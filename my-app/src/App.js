f2790c8f7fb6f75fffe121592e034f6040b2571b
jackychen8173 <jchen8173@gmail.com>
2024-11-18 16:33:44 -0800

    removed react stuff and added title and image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CanadaMap from "./assets/CanadaMap.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">
+        <h1 classNam="Title">Culturedle</h1>
+      </div>
+      <div className="Hints">
+        <img className="img" src={CanadaMap} alt="CanadaMap"/>
+      </div>
+      <div className="Textbox"></div>
     </div>
   );
 }
